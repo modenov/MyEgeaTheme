5d3a613acf2a50068a3e6ec028a4761de1d0a741
Vladimir Modenov <vmodenov@gmail.com>
2020-06-13 19:32:08 +0300

    My First Release of Theme

---
+++ theme-info.php
@@ -0,0 +1,21 @@
+<?php return array (
+
+  'index' => 9,
+
+  'display_name' => array (
+    'en' => 'My Theme',
+    'ru' => 'Моя тема',
+    'uk' => 'Моя тема',
+  ),
+
+  'colors' => array (
+    'background' => '#fff',
+    'headings' => '#000',
+    'text' => '#000',
+    'link' => '#0060a0',
+  ),
+
+  'meta_viewport' => 'width=device-width, initial-scale=1',
+  'supports_dark_mode' => true,
+
+); ?>
